b9ac9e191081acb356c8fad182940084541fc411
Saumya Ramani <raman@Saumya.com>
2025-03-03 10:16:27 +0530

    changes ma first

--- app.py
+++ app.py
@@ -1,224 +1,185 @@
-# import os
 import pandas as pd
-import numpy as np
-# import tensorflow as tf
-import whois
-from datetime import datetime
-from urllib.error import URLError
-# import socket
-# import ssl
-from urllib.parse import urlparse,urlencode
-import ipaddress
 import re
-import pickle
-
-from xgboost import XGBClassifier
-# from tensorflow.keras.models import load_model 
-
+# from bs4 import BeautifulSoup
+import whois
+import urllib
+import urllib.request
+import requests
+from datetime import datetime
 
 from flask import Flask, render_template, request
 
-with open("xgb_model.pkl", "rb") as model_file:
-    model = pickle.load(model_file) 
-
-# print(dir(model)) 
-
-
-# model = load_model("cnn.h5")
-# print(type(model))  # Check the type
 
 app = Flask(__name__)
 
-def check_domain_registration(url):
-    try:
-        # Remove http:// or https:// if present
-        domain = url.replace("http://", "").replace("https://", "").split('/')[0]
+# 1.Domain of the URL (Domain)
+from urllib.parse import urlparse
 
-        # Try to get domain information
-        domain_info = whois.whois(domain)
-
-        if domain_info.domain_name:
-            # print("Domain is registered")
-            # print(f"Registration Date: {domain_info.creation_date}")
-            # print(f"Expiry Date: {domain_info.expiration_date}")
-            return 0
-    except Exception as e:
-        # print(f"Domain is not registered or an error occurred: {e}")
-        return 1
-
-def domainAge(domain_name):
-    try:
-        domain_info = whois.whois(domain_name)
-    except Exception as e:
-        print(f"Error fetching WHOIS data: {e}")
-        return 1  # Assume suspicious if WHOIS lookup fails
-
-    creation_date = domain_info.creation_date
-    expiration_date = domain_info.expiration_date
-
-    # Handle cases where WHOIS data is not available
-    if creation_date is None or expiration_date is None:
-        return 1
-
-    # Handle cases where WHOIS returns lists
-    if isinstance(creation_date, list):
-        creation_date = creation_date[0]
-    if isinstance(expiration_date, list):
-        expiration_date = expiration_date[0]
-
-    # Ensure datetime format
-    if isinstance(creation_date, str):
-        try:
-            creation_date = datetime.strptime(creation_date, "%Y-%m-%d")
-        except ValueError:
-            return 1
-    if isinstance(expiration_date, str):
-        try:
-            expiration_date = datetime.strptime(expiration_date, "%Y-%m-%d")
-        except ValueError:
-            return 1
-
-    # Calculate domain age (from creation date to today)
-    ageofdomain = (datetime.now() - creation_date).days
-
-    # If the domain age is less than 6 months, return 1 (potentially suspicious)
-    return 1 if (ageofdomain / 30) < 6 else 0
-
-
-
-def domainEnd(domain_name):
-    try:
-        if domain_name is None:
-            return 1
-
-        expiration_date = domain_name.expiration_date
-        if expiration_date is None:
-            return 1
-
-        # Handle case where expiration_date is a list
-        if isinstance(expiration_date, list):
-            expiration_date = expiration_date[0]
-
-        if isinstance(expiration_date, str):
-            try:
-                expiration_date = datetime.strptime(expiration_date, "%Y-%m-%d")
-            except:
-                return 1
-
-        today = datetime.now()
-        end = abs((expiration_date - today).days)
-        if ((end/30) < 6):
-            return 0
-        return 1
-
-    except:
-        return 1
-    
-
-    # Domain of URL
 def getDomain(url):
   domain = urlparse(url).netloc
   if re.match(r"^www.",domain):
-    domain = domain.replace("www.","")
+	       domain = domain.replace("www.","")
   return domain
 
 
-# IP Address in URL
-def hasIP(url):
+# 2.Checks for IP address in URL (Have_IP)
+def havingIP(url):
   try:
     ipaddress.ip_address(url)
-    ip=1 # means ip is present in url -> It's phished
+    ip = 1
   except:
-    ip=0 # means ip is not present in url -> It's Legitimate
+    ip = 0
   return ip
 
 
-def hasSym(url):
-    if "@" in url:
-        temp = 1  # URL has '@' symbol -> It's phished
-    else:
-        temp = 0  # URL does not have '@' symbol -> It's Legitimate
-    return temp
-
-
-
-# Length of URL
-def getLen(url):
-  if(len(url) < 54):
-    temp=0 # It's legitimate
+# 3.Checks the presence of @ in URL (Have_At)
+def haveAtSign(url):
+  if "@" in url:
+    at = 1
   else:
-    temp=1 # Len is increased to hide doubtful part , It's Phished
-  return temp
+    at = 0
+  return at
 
 
-# Depth of URL (To get the no of '/')
+# 4.Finding the length of URL and categorizing (URL_Length)
+def getLength(url):
+  if len(url) < 54:
+    length = 0
+  else:
+    length = 1
+  return length
+
+
+# 5.Gives number of '/' in URL (URL_Depth)
 def getDepth(url):
   s = urlparse(url).path.split('/')
-  ct = 0
-  for i in range(len(s)):
-    if(len(s[i])!=0):
-      ct=ct+1
-  return ct
+  depth = 0
+  for j in range(len(s)):
+    if len(s[j]) != 0:
+      depth = depth+1
+  return depth
 
 
-# Redirection in URL ("//" must be present below 6th position)
-def redirect(url):
-    pos = url.rfind('//')
-    return 1 if pos > 7 else 0
+# 6.Checking for redirection '//' in the url (Redirection)
+def redirection(url):
+  pos = url.rfind('//')
+  if pos > 6:
+    if pos > 7:
+      return 1
+    else:
+      return 0
+  else:
+    return 0
 
 
-# HTTP/HTTPs in URL
-def checkHTTP(url):
+
+# 7.Existence of “HTTPS” Token in the Domain Part of the URL (https_Domain)
+def httpDomain(url):
   domain = urlparse(url).netloc
   if 'https' in domain:
-    return 1 # It's phished one
+    return 1
   else:
-    return 0 # It's Legitimate one
-  
+    return 0
 
-# URL Shortening
-shortening_services = [
-    "bit.ly", "tinyurl.com", "goo.gl", "ow.ly", "t.co", "is.gd", "buff.ly",
-    "adf.ly", "bit.do", "shorte.st", "clk.sh", "x.co", "tr.im", "goo.su"
-]
 
-def urlShortened(url):
-    domain = urlparse(url).netloc
-    return 1 if domain in shortening_services else 0 # 1 -> Phished one or 0 -> Legitimate one
 
-# "-" in Domain
-def checkPrefSuff(url):
-  domain = urlparse(url).netloc
-  if '-' in domain:
-    return 1 # Phished one
+#listing shortening services
+shortening_services = r"bit\.ly|goo\.gl|shorte\.st|go2l\.ink|x\.co|ow\.ly|t\.co|tinyurl|tr\.im|is\.gd|cli\.gs|" \
+                      r"yfrog\.com|migre\.me|ff\.im|tiny\.cc|url4\.eu|twit\.ac|su\.pr|twurl\.nl|snipurl\.com|" \
+                      r"short\.to|BudURL\.com|ping\.fm|post\.ly|Just\.as|bkite\.com|snipr\.com|fic\.kr|loopt\.us|" \
+                      r"doiop\.com|short\.ie|kl\.am|wp\.me|rubyurl\.com|om\.ly|to\.ly|bit\.do|t\.co|lnkd\.in|db\.tt|" \
+                      r"qr\.ae|adf\.ly|goo\.gl|bitly\.com|cur\.lv|tinyurl\.com|ow\.ly|bit\.ly|ity\.im|q\.gs|is\.gd|" \
+                      r"po\.st|bc\.vc|twitthis\.com|u\.to|j\.mp|buzurl\.com|cutt\.us|u\.bb|yourls\.org|x\.co|" \
+                      r"prettylinkpro\.com|scrnch\.me|filoops\.info|vzturl\.com|qr\.net|1url\.com|tweez\.me|v\.gd|" \
+                      r"tr\.im|link\.zip\.net"
+
+# 8. Checking for Shortening Services in URL (Tiny_URL)
+def tinyURL(url):
+    match=re.search(shortening_services,url)
+    if match:
+        return 1
+    else:
+        return 0
+
+
+
+# 9.Checking for Prefix or Suffix Separated by (-) in the Domain (Prefix/Suffix)
+def prefixSuffix(url):
+    if '-' in urlparse(url).netloc:
+        return 1            # phishing
+    else:
+        return 0            # legitimate
+
+
+
+# 13.Survival time of domain: The difference between termination time and creation time (Domain_Age)
+def domainAge(domain_name):
+  creation_date = domain_name.creation_date
+  expiration_date = domain_name.expiration_date
+  if (isinstance(creation_date,str) or isinstance(expiration_date,str)):
+    try:
+      creation_date = datetime.strptime(creation_date,'%Y-%m-%d')
+      expiration_date = datetime.strptime(expiration_date,"%Y-%m-%d")
+    except:
+      return 1
+  if ((expiration_date is None) or (creation_date is None)):
+      return 1
+  elif ((type(expiration_date) is list) or (type(creation_date) is list)):
+      return 1
   else:
-    return 0 # Legitimate one
-  
-# IFrame Redirection
-# If the iframe is empty or repsonse is not found then, the value assigned to this feature is 1 (phishing)
-# or else 0 (legitimate).
+    ageofdomain = abs((expiration_date - creation_date).days)
+    if ((ageofdomain/30) < 6):
+      age = 1
+    else:
+      age = 0
+  return age
+
+
+
+# 14.End time of domain: The difference between termination time and current time (Domain_End)
+def domainEnd(domain_name):
+  expiration_date = domain_name.expiration_date
+  if isinstance(expiration_date,str):
+    try:
+      expiration_date = datetime.strptime(expiration_date,"%Y-%m-%d")
+    except:
+      return 1
+  if (expiration_date is None):
+      return 1
+  elif (type(expiration_date) is list):
+      return 1
+  else:
+    today = datetime.now()
+    end = abs((expiration_date - today).days)
+    if ((end/30) < 6):
+      end = 0
+    else:
+      end = 1
+  return end
+
+
+# 15. IFrame Redirection (iFrame)
 def iframe(response):
   if response == "":
       return 1
   else:
-      if re.findall(r"[|]", response.text):
+      if re.findall(r"[<iframe>|<frameBorder>]", response.text):
           return 0
       else:
           return 1
-      
 
-# Status Bar Customization
+
+# 16.Checks the effect of mouse over on status bar (Mouse_Over)
 def mouseOver(response):
   if response == "" :
     return 1
   else:
-    if re.findall("", response.text):
+    if re.findall("<script>.+onmouseover.+</script>", response.text):
       return 1
     else:
       return 0
-    
 
-# Disable Right Click
+# 17.Checks the status of the right click attribute (Right_Click)
 def rightClick(response):
   if response == "":
     return 1
@@ -227,9 +188,8 @@ def rightClick(response):
       return 0
     else:
       return 1
-    
-# Website Forwarding
-# Taking assumption of visiting Legitimate site max one time
+
+# 18.Checks the number of forwardings (Web_Forwards)
 def forwarding(response):
   if response == "":
     return 1
@@ -238,74 +198,46 @@ def forwarding(response):
       return 0
     else:
       return 1
-    
 
+
+
+#Function to extract features
 def featureExtraction(url):
-    features = []
-    # Initialize domain_name first
-    dns = 0
-    try:
-        if not url.startswith(('http://', 'https://')):
-            url = 'http://' + url
-        domain_name = whois.whois(urlparse(url).netloc)
-    except:
-        dns = 1
-        domain_name = None
+  features = []
+  #Address bar based features (10)
+  features.append(getDomain(url))
+  features.append(havingIP(url))
+  features.append(haveAtSign(url))
+  features.append(getLength(url))
+  features.append(getDepth(url))
+  features.append(redirection(url))
+  features.append(httpDomain(url))
+  features.append(tinyURL(url))
+  features.append(prefixSuffix(url))
 
-    #Address bar based features (10)
-    # features.append(getDomain(url))
-    features.append(hasIP(url))
-    features.append(hasSym(url))
-    features.append(getLen(url))
-    features.append(getDepth(url))
-    features.append(redirect(url))
-    features.append(checkHTTP(url))
-    features.append(urlShortened(url))
-    features.append(checkPrefSuff(url))
-    features.append(check_domain_registration(url))
-    features.append(domainAge(domain_name))
-    features.append(domainEnd(domain_name))
+  #Domain based features (4)
+  dns = 0
+  try:
+    domain_name = whois.whois(urlparse(url).netloc)
+  except:
+    dns = 1
 
-    # HTML & Javascript based features
-    try:
-        response = requests.get(url)
-    except:
-        response = ""
-    features.append(iframe(response))
-    features.append(mouseOver(response))
-    features.append(rightClick(response))
-    features.append(forwarding(response))
+  features.append(dns)
+  features.append(1 if dns == 1 else domainAge(domain_name))
+  features.append(1 if dns == 1 else domainEnd(domain_name))
 
-    return features
+  # HTML & Javascript based features (4)
+  try:
+    response = requests.get(url)
+  except:
+    response = ""
+  features.append(iframe(response))
+  features.append(mouseOver(response))
+  features.append(rightClick(response))
+  features.append(forwarding(response))
 
+  return features
 
-
-
-
-def features_df(features_combined):
-    feature_names = ['Have_IP', 'Have_At', 'URL_Length', 'URL_Depth','Redirection',
-                 'https_Domain', 'TinyURL', 'Prefix/Suffix', 'DNS_Record',
-                 'Domain_Age', 'Domain_End', 'iFrame', 'Mouse_Over',
-                 'Right_Click', 'Web_Forwards']
-
-
-    final_df = pd.DataFrame([features_combined], columns=feature_names)
-    
-    # Convert object type columns to numerical values
-    for col in final_df.select_dtypes(include=['object']).columns:
-        final_df[col] = 1  # Assign 1 to all object-type values
-    
-    return final_df
-
-# import pandas as pd
-
-def predict_model(final):
-    final = pd.get_dummies(final, drop_first=True)
-
-
-    prediction = model.predict(final)
-    return prediction
-    # print(prediction)
 
 
 @app.route('/')
@@ -314,16 +246,11 @@ def home():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    url = request.form['url']  # Get the URL input from the form
-    features_rec=[]
-    features_rec2=[]
-    features_rec=featureExtraction(url)
-    features_rec=features_df(features_rec)
-    print(features_rec)
-    result=predict_model(features_rec)
-    print(features_rec)
-    return f"{features_rec}"
-    # return render_template('index2.html')
+   url = request.form['url']
+   features1=[]
+   features1=featureExtraction(url)
+   legi_features1 = legi_features[1:]
+   return render_template('index2.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
